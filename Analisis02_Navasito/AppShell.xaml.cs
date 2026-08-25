eb9c5a4ba085ee88f0b97f394ba233b06c2e9e9b
NAVAS2007 <quintanillacristian412@gmail.com>
2026-08-24 20:38:37 -0600

    App 2

---
+++ Analisis02_Navasito/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Analisis02_Navasito
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Analisis02_Navasito/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Analisis02_Navasito
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            MostrarCuentaRegresiva();
+        }
+
+        private void MostrarCuentaRegresiva()
+        {
+            string resultado = "";
+            for (int i = 10; i >= 1; i--)
+            {
+                resultado += i + " ";
+            }
+            ResultadosLabel.Text = resultado;
+        }
+
+    }
+}
