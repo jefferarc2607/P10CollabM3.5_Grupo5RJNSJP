1b16a9a8fe46422a37e50d1811fd8d23295e0026
paola <paola@PAO>
2026-08-27 07:54:22 -0600

    App 4 de pao

---
+++ Analisis04_paola/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Analisis04_paola
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularSueldo(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(HorasEntry.Text) || string.IsNullOrEmpty(PagoHoraEntry.Text))
+            {
+                ResultadoLabel.Text = "Favor de llenar ambos campos";
+                return;
+            }
+
+            double horas = double.Parse(HorasEntry.Text);
+            double pagoHora = double.Parse(PagoHoraEntry.Text);
+            double sueldo = horas * pagoHora;
+
+            ResultadoLabel.Text = "Sueldo semanal: $" + sueldo.ToString("0.00");
+        }
+    }
+}
