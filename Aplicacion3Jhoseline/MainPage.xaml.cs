ae28dd081ec322dae7677a15aecccce3b58795ca
Jhoseline <hoseline@DESKTOP-15V1RI2.com>
2026-08-27 09:08:15 -0600

    Aplicacion 3 Jhoseline

---
+++ Aplicacion3Jhoseline/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace Aplicacion3Jhoseline
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            MostrarTablas();
+        }
+
+        private void MostrarTablas()
+        {
+            string resultado = "";
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado += "Tabla del " + i + "\n";
+                for (int j = 1; j <= 10; j++)
+                {
+                    int multiplicacion = i * j;
+                    resultado += i + " x " + j + " = " + multiplicacion + "\n";
+                }
+                resultado += "\n";
+            }
+            ResultadosLabel.Text = resultado;
+        }
+    }
+}
---
+++ Aplicacion3Jhoseline/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Aplicacion3Jhoseline
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
